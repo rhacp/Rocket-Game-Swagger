039661e393be96f68831f5d6789adbdf34f9bf8e
Anghel Andrei <mirceanghel98@gmail.com>
2023-12-23 21:39:44 +0200

    Update swagger-initializer.js
    Changed the config file to `api-docs.yaml` instead of `swagger.yaml`.

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,8 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    //url: "swagger.yaml",
+    url: "api-docs.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
